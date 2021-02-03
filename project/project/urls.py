82aaa299ed09c53d96910ec8c8ed40243b0304b6
Andrew Miller <206alm@gmail.com>
2021-02-02 16:40:43 -0800

    add sign up forms, mid styles

--- project/project/urls.py
+++ project/project/urls.py
@@ -21,7 +21,7 @@ from board_room import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signup', accounts_views.signup, name='signup'),
+    path('signup/', accounts_views.signup, name='signup'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
